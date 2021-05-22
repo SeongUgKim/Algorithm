518894076b81d9dc3fac3f5e7f144abac15597c0
SeongUgKim <sukim2017@gmail.com>
2021-05-22 15:35:50 +0900

    Same Tree (need to fill out Iterative way after learning BFS

---
+++ Tree/SameTree.cpp
@@ -0,0 +1,75 @@
+class TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+};
+
+// Preorder check
+void helper(vector<TreeNode*>& vec, TreeNode* p)
+{
+    if (p == nullptr)
+    {
+        vec.push_back(nullptr);
+        return;
+    }
+    vec.push_back(p);
+    helper(vec, p->left);
+    helper(vec, p->right);
+}
+
+bool isSameTree(TreeNode* p, TreeNode* q) {
+    vector<TreeNode*> vec1;
+    vector<TreeNode*> vec2;
+    helper(vec1, p);
+    helper(vec2, q);
+    int m = vec1.size();
+    int n = vec2.size();
+    if (m != n)
+    {
+        return false;
+    }
+    for (int i = 0; i < m; ++i)
+    {
+        if (vec1[i] == nullptr)
+        {
+            if (vec2[i] != nullptr)
+            {
+                return false;
+            }
+            continue;
+        }
+        if (vec2[i] == nullptr)
+        {
+            if (vec1[i] != nullptr)
+            {
+                return false;
+            }
+            continue;
+        }
+        if (vec1[i]->val != vec2[i]->val)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Recursive
+bool isSameTreeRecursive(TreeNode* p, TreeNode* q)
+{
+    if (p == nullptr && q == nullptr)
+    {
+        return true;
+    }
+    if (p == nullptr || q == nullptr)
+    {
+        return false;
+    }
+    if (p->val != q->val)
+    {
+        return false;
+    }
+    return isSameTreeRecursive(p->left, q->left) && isSameTreeRecursive(p->right, q,right);
+}
+
+// Iterative After Learning BFS
