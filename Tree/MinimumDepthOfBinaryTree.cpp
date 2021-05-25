0a386e08bf849edfa64f1876b87e598eb80367ed
SeongUgKim <sukim2017@gmail.com>
2021-05-26 02:48:46 +0900

    2021.05.26 Leetcode #111

---
+++ Tree/MinimumDepthOfBinaryTree.cpp
@@ -0,0 +1,20 @@
+class TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+};
+
+// Recursive
+int minDepth(TreeNode* root) {
+    if (root == nullptr)
+    {
+        return 0;
+    }
+    if (root->left == nullptr || root->right == nullptr)
+    {
+        return max(minDepth(root->right), minDepth(root->left)) + 1;
+    }
+    return min(minDepth(root->left), minDepth(root->right)) + 1;
+}
+
+
