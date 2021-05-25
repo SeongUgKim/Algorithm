9b2dd680dd6ed7dd686695090d9b51a2570858bb
SeongUgKim <sukim2017@gmail.com>
2021-05-26 00:52:19 +0900

    2021.05.26 Leetcode #110

---
+++ Tree/BalancedBinaryTree.cpp
@@ -0,0 +1,30 @@
+class TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+};
+
+int countLevel(TreeNode* root)
+{
+    if (root == nullptr)
+    {
+        return 0;
+    }
+    int left = countLevel(root->left) + 1;
+    int right = countLevel(root->right) + 1;
+    return max(left, right);
+}
+bool isBalanced(TreeNode* root) {
+    if (root == nullptr)
+    {
+        return true;
+    }
+    int diff = abs(countLevel(root->right) - countLevel(root->left));
+    if (diff > 1)
+    {
+        return false;
+    }
+    return  isBalanced(root->left) && isBalanced(root->right);
+}
+
+
