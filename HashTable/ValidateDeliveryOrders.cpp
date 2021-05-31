ad409ca6dd3369c514b9a5d66ec3a8b980f5c426
SeongUgKim <sukim2017@gmail.com>
2021-06-01 02:24:07 +0900

    Leetcode 06.01

--- HashTable/ValidateDeliveryOrders.cpp
+++ HashTable/ValidateDeliveryOrders.cpp
@@ -1,54 +1,30 @@
 #include <unordered_map>
-#include <unordered_set>
 #include <vector>
 using namespace std;
 bool solve(vector<string>& orders) {
     unordered_map<int, int> mp;
-    unordered_set<string> st;
-    for (string order : orders)
+    int check = 0;
+    for (auto order : orders)
     {
-        st.insert(order);
-    }
-    if (st.size() != orders.size())
-    {
-        return false;
-    }
-    st.clear();
-    for (string order: orders)
-    {
+        int val = stoi(order.substr(1));
         if (order[0] == 'P')
         {
-            string code = "";
-            for (int i = 1; i < order.size(); ++i)
+            if (mp.count(val))
             {
-                code += order[i];
+                return false;
             }
-            int number = stoi(code);
-            mp[number]++;
-            continue;
+            mp[val]++;
+            check++;
         }
-        string code = "";
-        for (int i = 1; i < order.size(); ++i)
+        else
         {
-            code += order[i];
-        }
-        int number = stoi(code);
-        if (mp[number] == 0)
-        {
-            return false;
-        }
-        if (mp[number] == 1)
-        {
-            mp[number]--;
-            continue;
+            if (mp[val] != 1)
+            {
+                return false;
+            }
+            mp[val]++;
+            check--;
         }
     }
-    for (auto m : mp)
-    {
-        if (m.second != 0)
-        {
-            return false;
-        }
-    }
-    return true;
+    return check == 0;
 }
