262392f6f283868bf69dfc21ce94317ab5ba1d8e
Seong Ug Kim <sukim2017@gmail.com>
2022-01-19 06:36:38 -0500

    longest palindromic substring

--- DynamicProgramming/LongestPalindromicSubstring.cpp
+++ DynamicProgramming/LongestPalindromicSubstring.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 string longestPalindrome(string s) {
     int n = s.size();
-    int dp[n][n];
+    int dp[1001][1001];
     memset(dp, 0, sizeof(dp));
     string ans = "";
     ans = s[0];
     for (int i = 0; i < n; ++i) {
         dp[i][i] = 1;
     }
-    for (int i = n - 1; i >= 0; --i) {
+    for (int i = n - 2; i >= 0; --i) {
         for (int j = i + 1; j < n; ++j) {
             if (s[i] == s[j]) {
                 if (j - i == 1 || dp[i + 1][j - 1] == 1) {
